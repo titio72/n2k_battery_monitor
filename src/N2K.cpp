68ea77f98d71a15b645dfe6a7a825ab7be0884eb
Andrea Boni <andrboni@gmail.com>
2022-01-12 09:52:01 +0100

    Added license to each file

--- src/N2K.cpp
+++ src/N2K.cpp
@@ -1,3 +1,18 @@
+/*
+(C) 2022, Andrea Boni
+This file is part of n2k_battery_monitor.
+n2k_battery_monitor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+NMEARouter is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with n2k_battery_monitor.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifdef ESP32_ARCH
 #define USE_N2K_CAN USE_N2K_ESP32_CAN
 #define ESP32_CAN_TX_PIN GPIO_NUM_5  // Set CAN TX port to 5 
